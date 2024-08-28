77b0ee195aed7222293c74ad4650780911e771d2
siam3h <philosiama@gmail.com>
2024-08-29 00:17:14 +0300

    Update import_export modules,commands,tests

--- app/commands.py
+++ app/commands.py
@@ -1,10 +1,9 @@
 import argparse
-from .cleaning_cmd import cleaning_command
-from .transformation_cmd import transformation_command
-from .analysis_cmd import analysis_command
-from .visualization_cmd import visualization_command
-from .import_export_cmd import import_export_command
-from .reporting_cmd import reporting_command
+from ..app.cli.cleaning_cmd import cleaning_command
+from ..app.cli.transformation_cmd import transformation_command
+from ..app.cli.visualization_cmd import visualization_command
+from ..app.cli.import_export_cmd import import_export_command
+from ..app.cli.reporting_cmd import reporting_command
 
 def cli():
     parser = argparse.ArgumentParser(description="CLI tool for automating Excel/CSV data tasks.")
@@ -13,7 +12,6 @@ def cli():
 
     cleaning_command(subparsers)
     transformation_command(subparsers)
-    analysis_command(subparsers)
     visualization_command(subparsers)
     import_export_command(subparsers)
     reporting_command(subparsers)
