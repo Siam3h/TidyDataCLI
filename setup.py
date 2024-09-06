d4143beaadd47e18744b70ca40cdc66feee13d1c
siam3h <philosiama@gmail.com>
2024-09-06 21:00:02 +0300

    Bug Fix:Package Structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
  name="TidyDataCLI",
- version="0.2.5",
+ version="0.2.6",
  packages=find_packages(),
  install_requires=[
         "pandas==2.2.2",
