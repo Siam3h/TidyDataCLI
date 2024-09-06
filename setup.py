576d96d7a5cdfee7d334bac9f3bd37ac33fb05bc
siam3h <philosiama@gmail.com>
2024-09-06 21:18:27 +0300

    Bug fix: entry point CLI

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
  name="TidyDataCLI",
- version="0.2.6",
+ version="0.2.8",
  packages=find_packages(),
  install_requires=[
         "pandas==2.2.2",
@@ -60,7 +60,7 @@ setup(
     ],
  entry_points={
      "console_scripts": [
-         'tidydata = src.cmd:main',
+         'tidydata = src.cmd:cli',
      ],
  },
  test_suite='tests',
